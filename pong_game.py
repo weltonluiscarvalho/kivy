9f8db7a51230ca977a7b015f6d9f0b9a837ae617
Welton Carvalho <welton.carvalho.dev@gmail.com>
2025-09-07 11:48:50 -0300

    feat: create pause_game and end_game funcionality

--- pong_game.py
+++ pong_game.py
@@ -2,7 +2,6 @@ from kivy.app import App
 from kivy.clock import Clock
 from kivy.uix.widget import NumericProperty, ObjectProperty, ReferenceListProperty, Widget
 from kivy.vector import Vector
-from random import randint
 
 class PongPaddle(Widget):
 
@@ -36,6 +35,41 @@ class PongGame(Widget):
     ball = ObjectProperty(None)
     player1 = ObjectProperty(None)
     player2 = ObjectProperty(None)
+    button_pause = ObjectProperty(None)
+    label_wins = ObjectProperty(None)
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.button_pause.bind(on_press=self.on_press_pause_button)
+        self.is_running = True
+
+    def on_end_game(self, player):
+        self.label_wins.text = f"player{player} wins the game"
+        self.label_wins.opacity = 1.0
+        self.ball.center = self.center
+        self.is_running = False
+        self.disable_childs()
+        self.button_pause.text = "Start New Game"
+        self.player2.score = 0
+        self.player1.score = 0
+
+    def disable_childs(self):
+        self.ball_velocity = self.ball.velocity_x, self.ball.velocity_y
+        self.ball.velocity = 0, 0
+        self.button_pause.text = "Restart"
+
+    def enable_childs(self):
+        self.label_wins.opacity = 0.0
+        self.ball.velocity = self.ball_velocity
+        self.button_pause.text = "Pause"
+
+    def on_press_pause_button(self, event):
+        if self.is_running:
+            self.is_running = False
+            self.disable_childs()
+        else:
+            self.is_running = True
+            self.enable_childs()
 
     def on_touch_move(self, touch):
         if touch.x < self.width/3:
@@ -65,10 +99,16 @@ class PongGame(Widget):
         # went off to a side to score point?
         if self.ball.x < self.x:
             self.player2.score += 1
-            self.serve_ball(vel=(4, 0))
+            if self.player2.score >= 3:
+                self.on_end_game(2)
+            else:
+                self.serve_ball(vel=(4, 0))
         if self.ball.right > self.width:
             self.player1.score += 1
-            self.serve_ball(vel=(-4, 0))
+            if self.player1.score >= 3:
+                self.on_end_game(1)
+            else:
+                self.serve_ball(vel=(-4, 0))
 
 class PongApp(App):
     def build(self):
