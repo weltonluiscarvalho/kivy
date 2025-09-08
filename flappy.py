161d56d5407a158f94d5a7920da4e4c1ac920a6c
Welton Carvalho <welton.carvalho.dev@gmail.com>
2025-09-07 21:07:02 -0300

    feat: bounce the ball

--- flappy.py
+++ flappy.py
@@ -2,6 +2,8 @@ from kivy.app import App
 from kivy.uix.floatlayout import FloatLayout
 from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.uix.image import Image
+from kivy.clock import Clock
+from kivy.properties import NumericProperty
 
 class Manager(ScreenManager):
     pass
@@ -10,10 +12,18 @@ class Menu(Screen):
     pass
 
 class Game(Screen):
-    pass
+    def on_enter(self, *args):
+        Clock.schedule_interval(self.update, 1/30)
+
+    def update(self, *args):
+        self.ids.player.speed += -self.height * 1/30
+        self.ids.player.y += self.ids.player.speed * 1/30
+
+    def on_touch_down(self, *args):
+        self.ids.player.speed = self.height*0.7
 
 class Player(Image):
-    pass
+    speed = NumericProperty(0)
 
 class ArcBird(App):
     pass
