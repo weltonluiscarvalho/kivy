e665431b74c09ad3c7a6fe744b6a51c219143564
Welton Carvalho <welton.carvalho.dev@gmail.com>
2025-09-07 21:47:46 -0300

    feat: create obstacles

--- flappy.py
+++ flappy.py
@@ -3,7 +3,9 @@ from kivy.uix.floatlayout import FloatLayout
 from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.uix.image import Image
 from kivy.clock import Clock
-from kivy.properties import NumericProperty
+from kivy.properties import NumericProperty, ListProperty
+from kivy.uix.widget import Widget
+from kivy.animation import Animation
 
 class Manager(ScreenManager):
     pass
@@ -12,8 +14,17 @@ class Menu(Screen):
     pass
 
 class Game(Screen):
+
+    obstacles = []
+
     def on_enter(self, *args):
         Clock.schedule_interval(self.update, 1/30)
+        Clock.schedule_interval(self.put_obstacle, 1)
+
+    def put_obstacle(self, *args):
+        obstacle = Obstacle(x=self.width, height=400)
+        self.add_widget(obstacle)
+        self.obstacles.append(obstacle)
 
     def on_pre_enter(self, *args):
         self.ids.player.y = self.height / 2
@@ -30,10 +41,30 @@ class Game(Screen):
 
     def game_over(self):
         Clock.unschedule(self.update, 1/30)
+        Clock.unschedule(self.put_obstacle, 1)
+        for ob in self.obstacles:
+            ob.anim.cancel(ob)
+            self.remove_widget(ob)
+        self.obstacles = []
         App.get_running_app().root.current = 'game_over'
+
 
 class Player(Image):
     speed = NumericProperty(0)
+    
+class Obstacle(Widget):
+    color = ListProperty([0.3, 0.2, 0.2, 1])
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.anim = Animation(x=-self.width, duration=3)
+        self.anim.bind(on_complete=self.vanish)
+        self.anim.start(self)
+
+    def vanish(self, *args):
+        game_screen = App.get_running_app().root.get_screen('game')
+        game_screen.remove_widget(self)
+        game_screen.obstacles.remove(self)
 
 class GameOver(Screen):
     pass
