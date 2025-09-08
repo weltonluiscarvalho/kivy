ef23f78ab0ed28b03439ede3c9714d9aae824369
Welton Carvalho <welton.carvalho.dev@gmail.com>
2025-09-07 21:28:35 -0300

    feat: create game over screen

--- flappy.py
+++ flappy.py
@@ -15,15 +15,28 @@ class Game(Screen):
     def on_enter(self, *args):
         Clock.schedule_interval(self.update, 1/30)
 
+    def on_pre_enter(self, *args):
+        self.ids.player.y = self.height / 2
+        self.ids.player.speed = 0
+
     def update(self, *args):
         self.ids.player.speed += -self.height * 1/30
         self.ids.player.y += self.ids.player.speed * 1/30
+        if self.ids.player.y > self.height or self.ids.player.y < 0:
+            self.game_over()
 
     def on_touch_down(self, *args):
         self.ids.player.speed = self.height*0.7
 
+    def game_over(self):
+        Clock.unschedule(self.update, 1/30)
+        App.get_running_app().root.current = 'game_over'
+
 class Player(Image):
     speed = NumericProperty(0)
+
+class GameOver(Screen):
+    pass
 
 class ArcBird(App):
     pass
