65236ca4ac78ef71430ac1ae7cfcabbdaa0b128f
Welton Carvalho <welton.carvalho.dev@gmail.com>
2025-09-09 11:10:07 -0300

    feat: finalize instagram_clone

--- instagram_clone/main.py
+++ instagram_clone/main.py
@@ -14,6 +14,12 @@ class InstagramApp(MDApp):
         Builder.load_file('libs/screens/home_page.kv')
         Builder.load_file('libs/components/appbar.kv')
         Builder.load_file('libs/components/story_creator.kv')
+        Builder.load_file('libs/components/bottom_nav.kv')
+        Builder.load_file('libs/components/circular_avatar_image.kv')
+        Builder.load_file('libs/components/post_card.kv')
+
+    def on_start(self):
+        self.root.dispatch('on_enter')
 
 if __name__ == "__main__":
     InstagramApp().run()
